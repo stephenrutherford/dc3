dce635c76dbd5e2daa9d0ea6d6fbf6d3ea670c3c
Stephen <survivingthequiet@gmail.com>
2022-04-05 10:53:24 +0100

    _document update for umami

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,14 @@
 import "../styles/globals.css";
-import Script from "next/script";
+// import Script from "next/script";
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
-      <Script
+      {/* <Script
         src="https://umami-five-sigma.vercel.app/blue.js"
         data-website-id="05729253-441e-433e-9439-d19170fac7f6"
         strategy="lazyOnload"
-      />
+      /> */}
       <Component {...pageProps} />
     </>
   );
